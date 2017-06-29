fc0657047edcc83523c83a927f1b2030aaaf507b
galarraga <galarraga@luis-station>
2017-06-29 18:16:00 +0200

    File parser parent class

---
+++ 4Bench/include/parsing/FileParser.hpp
@@ -0,0 +1,23 @@
+/*
+ * FileParser.hpp
+ *
+ *  Created on: Jun 29, 2017
+ *      Author: galarraga
+ */
+
+#ifndef PARSING_FILEPARSER_HPP_
+#define PARSING_FILEPARSER_HPP_
+
+namespace fourbench {
+namespace parsing {
+
+class FileParser {
+public:
+	FileParser();
+	virtual ~FileParser();
+};
+
+} /* namespace parsing */
+} /* namespace fourbench */
+
+#endif /* PARSING_FILEPARSER_HPP_ */
---
+++ 4Bench/src/parsing/FileParser.cpp
@@ -0,0 +1,23 @@
+/*
+ * FileParser.cpp
+ *
+ *  Created on: Jun 29, 2017
+ *      Author: galarraga
+ */
+
+#include "FileParser.hpp"
+
+namespace fourbench {
+namespace parsing {
+
+FileParser::FileParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FileParser::~FileParser() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace parsing */
+} /* namespace fourbench */
